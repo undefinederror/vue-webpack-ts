fbbeae92817308fe48296f4de497adf33f632e2c
undefinederror <github@object.ninja>
2017-06-06 15:38:21 +0200

    added require to sfc.f.ts

--- template/sfc.d.ts
+++ template/sfc.d.ts
@@ -2,3 +2,8 @@ declare module "*.vue" {
   import Vue from 'vue'
   export default typeof Vue
 }
+declare const require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name: string) => void;
+};
